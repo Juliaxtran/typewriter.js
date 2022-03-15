14aefc7720b008cf31e1a0294c494dfad6aedb2b
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-15 12:51:23 -0400

    Typewriter function

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs";
+const myTypewriter = () => {
+  let timer = 0;
+  for (const char of sentence) {
+  timer += 50; 
+    setTimeout(() =>
+      process.stdout.write(char), timer);
+
+
+  }
+}
+
+
+
+
+
+myTypewriter();
